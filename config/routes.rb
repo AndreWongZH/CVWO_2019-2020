a714aa282643ecc462bc4b5d9b49c2cb2dd0904b
Andre Wong <wongandre98@gmail.com>
2019-12-27 21:31:07 +0800

    modified add and focus routes so that it will not conflict with API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '*page', to: 'static#index', constraints: -> (req) do
+  get 'add', to: 'static#index', constraints: -> (req) do
     !req.xhr? && req.format.html?
+  end
+
+  get 'focus', to: 'static#index', constraints: -> (req) do
+  !req.xhr? && req.format.html?
   end
 
   resources :todos
 
   root 'static#index'
+
 end
