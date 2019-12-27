a31a9c29022f11a3d2ce93187d94ee531fceac81
Andre Wong <wongandre98@gmail.com>
2019-12-27 15:38:12 +0800

    added database initialization

---
+++ db/migrate/20191227071938_create_todos.rb
@@ -0,0 +1,14 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.date :created
+      t.date :deadline
+      t.text :desc
+      t.boolean :done
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+end
