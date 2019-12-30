567115642ddc17eb9ceadd3c6e4e5eb00ca8fe5c
Andre Wong <wongandre98@gmail.com>
2019-12-30 11:19:32 +0800

    updated ui for focus page and connected to backend API

---
+++ app/javascript/components/Focus/CategoryHeader/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Segment, Header, Icon } from 'semantic-ui-react'
+
+const CategoryHeader = ({ type, hide, iconName, headingName, focusCategory }) => (
+    <Segment.Inline>
+        <Header as='h3' floated='left'>
+            <Icon name={iconName}/>
+            <Header.Content>{headingName}</Header.Content>
+            
+        </Header>
+        <Header floated='right'>
+            <Icon link onClick={hide(type)} name={focusCategory ? "caret up" : "caret down"} />
+        </Header>
+    </Segment.Inline>
+)
+
+export default CategoryHeader
---
+++ app/javascript/components/Focus/ItemSegment/EachItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Item, Label } from 'semantic-ui-react'
+
+import LabelCell from '../../../TodoTable/TableRow/LabelCell'
+
+
+const EachItem = ({ data }) => {
+
+    const ItemCombined = data.map((todo) => 
+        <Item key={ todo.id }>
+
+            <Item.Content>
+                <Item.Header>{ todo.title }</Item.Header>
+                <Item.Meta>
+                    <span>Created on: { todo.created }</span>
+                </Item.Meta>
+                <Item.Description>{todo.desc}</Item.Description>
+                <Item.Extra>
+                    <LabelCell data={todo.tag} />
+                </Item.Extra>
+            </Item.Content>
+        </Item>
+    )
+
+    return ItemCombined
+}
+
+export default EachItem
