d084bc74d1e4d70c2f0bf234ac5a638be026b70e
Andre Wong <wongandre98@gmail.com>
2020-01-14 18:41:36 +0800

    added in authentication routes and redirected all http routes to static#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/google_oauth2/callback', to: 'session#create'
 
-  get 'add', to: 'static#index', constraints: -> (req) do
-    !req.xhr? && req.format.html?
-  end
+  get 'logged_in', to: 'session#logged_in'
 
-  get 'focus', to: 'static#index', constraints: -> (req) do
-  !req.xhr? && req.format.html?
-  end
+  get 'logout', to: 'session#destroy'
 
-  get 'edit/:id', to: 'static#index', constraints: -> (req) do
-  !req.xhr? && req.format.html?
-  end
+  resource :session, only: [:create, :destroy]
 
   get '/todos/focus', to: 'todos#focus'
 
   get '/todos/tags', to: 'todos#tags'
 
   resources :todos
+
+  get '*page', to: 'static#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
 
   root 'static#index'
 
